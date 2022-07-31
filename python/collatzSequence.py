385bb5f788c59d04060f03e778ce9a4eda0afad3
Josiahwirick <110127956+Josiahwirick@users.noreply.github.com>
2022-07-31 15:25:58 -0600

    Add files via upload

---
+++ python/collatzSequence.py
@@ -0,0 +1,30 @@
+import time
+
+
+def collatz(): # The function that does the math
+    print('Input a number:')
+    good = True
+    while good:
+        try: # Check if input works as an integer
+            number = int(input())
+            print(number)
+            int(number)
+            good = False
+        except ValueError:
+            print('Try again with a whole number:')
+            continue # Return back to input
+        
+        while number != 1: # Run the following until the number variable is equal to 1
+        
+            if (number % 2) == 0: # Check if the number variable is even
+                number = (number//2)
+                print(number)
+                time.sleep(0.3)
+    
+            elif (number % 2) == 1: # Check if number variable is odd
+                number = (number*3 + 1)
+                print(number)
+                time.sleep(0.3)
+            
+    print('Finished')
+collatz()
