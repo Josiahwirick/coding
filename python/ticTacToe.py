64c7ea3c39103a19fdbe611335565a2b36ff25e0
Josiahwirick <110127956+Josiahwirick@users.noreply.github.com>
2022-11-05 17:33:49 -0600

    Add files via upload

---
+++ python/ticTacToe.py
@@ -0,0 +1,28 @@
+theBoard = {'top-L': ' ', 'top-M': ' ', 'top-R': ' ',
+             'mid-L': ' ', 'mid-M': ' ', 'mid-R': ' ',
+             'low-L': ' ', 'low-M': ' ', 'low-R': ' ',}
+
+def printBoard(board):
+    print(board['top-L'] + '|' + board['top-M'] + '|' + board['top-R'])
+    print('-+-+-')
+    print(board['mid-L'] + '|' + board['mid-M'] + '|' + board['mid-R'])
+    print('-+-+-')
+    print(board['low-L'] + '|' + board['low-M'] + '|' + board['low-R'])
+
+turn = 'X'
+for i in range(9):
+    printBoard(theBoard)
+    print('Turn for ' + turn + '. Move on which space?')
+    move = input()
+    theBoard[move] = turn
+    if turn == 'X':
+        turn = 'O'
+    else:
+        turn = 'X'
+
+printBoard(theBoard)
+
+### Not a complete game setup, but it models the data structure for it. To make it a complete game;
+### 1. Define a "win" condition
+### 2. Don't let moves overwrite each other
+### 3. Figure out better input system than 'mid-M' to reduce possible input errors
