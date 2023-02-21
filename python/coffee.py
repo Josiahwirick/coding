b23f563280abe66f0f90a16924199801950da436
Josiah <saihplays.biz@gmail.com>
2023-02-21 00:42:55 -0700

    Just a fun little for loop

---
+++ python/coffee.py
@@ -0,0 +1,8 @@
+import time
+
+print('I could really use a lot of coffee.')
+
+for cup in range(1, 11):
+    print("Sipping down coffee number " + str(cup) + "...")
+    time.sleep(2)
+print("Why is the coffee always gone?")
